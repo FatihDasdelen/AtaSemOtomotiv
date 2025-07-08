5a62668a0b1382e8a5bcbd8798004a7016f04684
fatih daşdelen <fatihdasdelen@hotmail.com>
2025-07-08 18:38:49 +0300

    Add project files.

---
+++ AtaSemOtomotiv/wwwroot/js/script.js
@@ -0,0 +1,9 @@
+﻿// Slider için görseller arası geçiş
+let index = 0;
+const images = ['images/car1.jpg', 'images/car2.jpg', 'images/car3.jpg'];
+
+setInterval(() => {
+    const img = document.querySelector('.gallery img');
+    img.src = images[index];
+    index = (index + 1) % images.length;
+}, 3000);
